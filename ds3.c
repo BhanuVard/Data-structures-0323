21e42eabb9e87f991dc406648a9643b51712e295
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-05-02 21:21:00 +0530

    even or odd

---
+++ ds3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int n;
+	printf("enter the number");
+	scanf("%d",&n);
+	if(n%2==0)
+	{
+	printf("the num is even:");
+	}
+	else
+	{
+	printf("the num is odd:"); 
+}
+return 0;
+}
